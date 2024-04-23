902bff8e11a62c64c130683256855f62d1a2417c
砚池/Ivan <yanchi.zyc@bytedance.com>
2024-04-23 11:57:45 +0800

    🚀 Deploy stuff.
    Change-Id: I6e76bdf16471e699f38c9218f96d5695f94482d2

--- re_test.go
+++ re_test.go
@@ -6,7 +6,10 @@
 
 package re
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func TestRe(t *testing.T) {
 	expression := ExpressionBuilder().
@@ -18,6 +21,39 @@ func TestRe(t *testing.T) {
 		Build()
 	println(expression.String())
 	println(expression.regexp.MatchString("ababababababa"))
-	str := `abacad`
+	str := `aba`
+	t.Log(expression.GetText(str, "foo"))
+}
+
+func TestModifier(t *testing.T) {
+	r := regexp.MustCompile(`(?ism)CaSe`)
+	res := r.MatchString("case")
+	t.Log(res)
+
+	expression := ExpressionBuilder().
+		StartOfLine().
+		Then("CaSe").
+		Build()
+	t.Log(expression.String())
+	t.Log(expression.Test("CaSe"))
+	t.Log(expression.Test("case"))
+	t.Log(expression.Test("CASE"))
+}
+
+func TestGroup(t *testing.T) {
+	str := `abcaAabbbxxxx`
+	expression := ExpressionBuilder().
+		StartOfLine().
+		Anything().
+		CaptWithName("foo").
+		Then("a").Count(2).
+		EndCapt().
+		Anything().
+		EndOfLine().
+		WithAnyCase().
+		Build()
+
+	t.Log(expression.String())
+	t.Log(expression.Test(str))
 	t.Log(expression.GetText(str, "foo"))
 }
